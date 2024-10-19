63bacebf0976762baaf52dd90a2ba7684ca97a60
yudhabahrul <yudhabahrul40@gmail.com>
2024-10-19 18:09:50 +0700

    memperbarui kode

--- firebase.ts
+++ firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw3ilT9AjhINcbUGGzyMStS6uccZSLkTI",
-  authDomain: "next-chat-app-7d85b.firebaseapp.com",
-  projectId: "next-chat-app-7d85b",
-  storageBucket: "next-chat-app-7d85b.appspot.com",
-  messagingSenderId: "728016960645",
-  appId: "1:728016960645:web:0584125f6bf137821b7dec",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
